b48ff008192e6ae0499eb2a070d2fb0b112e1d32
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-09 16:56:29 -0700

    completed view products front end | Bailey Hill

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 class ProductListItem extends React.Component {
   render() {
     return (
-      // <div className="card-deck">
-      // <div className="col w-25 p-3">
       <div className="w-25 m-3 card">
         <div className="card-body">
-          <h5 className="card-title">{this.props.name}</h5>
-          <img className="images card-img" src={this.props.image}/>
-          <h6 className="card-subtitle">{this.props.price}</h6>
-          <p className="card-text">{this.props.shortDescription}</p>
+          <div className="img-container">
+            <img className="images card-img-top" src={this.props.image} />
+          </div>
+          <div className="card-body-text-container">
+            <h5 className="card-title">{this.props.name}</h5>
+            <h6 className="card-subtitle">{`$ ${(this.props.price / 100).toFixed(2)}`}</h6>
+            <p className="card-text">{this.props.shortDescription}</p>
+          </div>
         </div>
       </div>
-      // </div>
-      // </div>
     );
   }
 }
