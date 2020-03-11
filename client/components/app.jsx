36e5ac45fba6c04fb82a7dd70f78f369d3700417
bailey-hill <bhill1@mail.sfsu.edu>
2020-03-11 15:47:40 -0700

    added addtocart method to app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -16,6 +16,8 @@ class App extends React.Component {
     };
     this.setView = this.setView.bind(this);
     this.getCartItems = this.getCartItems.bind(this);
+    this.cartItemCount = this.cartItemCount.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   componentDidMount() {
@@ -39,14 +41,28 @@ class App extends React.Component {
       });
   }
 
-  cardItemCount() {
+  cartItemCount() {
     const data = this.state.cart;
     let total = 0;
     for (let i = 0; i < data.length; i++) {
-      total += data[i].product;
+      total += data[i];
     }
     Number(total);
     return total;
+  }
+
+  addToCart(product) {
+    fetch('/api/cart/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(product)
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          cart: this.state.cart.concat(data)
+        });
+      });
   }
 
   render() {
@@ -56,7 +72,7 @@ class App extends React.Component {
     if (viewName === 'catalog') {
       return (
         <div>
-          <Header cardItemCount={this.cardItemCount()}/>
+          <Header cardItemCount={this.cartItemCount()}/>
           <ProductList setView={setView}/>
         </div>
       );
@@ -64,7 +80,7 @@ class App extends React.Component {
       return (
         <div>
           <Header />
-          <ProductDetails product={this.props.product} setView={setView} viewParams={viewParams} />
+          <ProductDetails addToCart={this.addToCart} product={this.props.product} setView={setView} viewParams={viewParams} />
         </div>
       );
     }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="d-flex justify-content-space-between navbar navbar-dark bg-dark">
         <h2 className="navbar-brand ml-5">$Wicked Sales</h2>
-        <h2 className="navbar-brand ml-5">Item Count: {this.props.cardItemCount}</h2>
+        <h2 className="navbar-brand mr-5"><span>
+          {this.props.cartItemCount}</span> Item <i className="fas fa-shopping-cart"></i></h2>
       </nav>
     );
   }
