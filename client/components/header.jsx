9569266e8329145cfe1179b674eb1d47f31809f1
bailey-hill <bhill1@mail.sfsu.edu>
2020-04-02 16:10:24 -0700

    fixed mobile responsiveness for list, added pointers, and form validation

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -14,7 +14,7 @@ class Header extends React.Component {
     return (
       <nav className="d-flex justify-content-space-between navbar navbar-dark bg-dark">
         <h2 className="navbar-brand ml-5">$Wicked Sales</h2>
-        <h2 onClick={this.goToCart} className="navbar-brand mr-5" ><span>
+        <h2 onClick={this.goToCart} className="pointer navbar-brand mr-5" ><span>
           {this.props.cartItemCount}</span> Item <i className="fas fa-shopping-cart"></i></h2>
       </nav>
     );
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class ProductListItem extends React.Component {
   render() {
     return (
-      <div id={this.props.productId} onClick={this.props.goToDetails} className="w-25 m-3 card d-flex align-items-center justify-content-center">
+      <div id={this.props.productId} onClick={this.props.goToDetails} className="pointer card-width m-3 card d-flex align-items-center justify-content-center">
         <div className="card-body p-0 d-flex flex-column justify-content-around">
-          <div className="img-container">
+          <div className="img-container p-2">
             <img className="card-img-top" src={this.props.image} />
           </div>
           <div className="d-flex justify-content-space-between align-items-start flex-column">
