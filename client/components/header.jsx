ecf5dc992c5ce901de626c2f71849dd542b05381
bailey-hill <bhill1@mail.sfsu.edu>
2020-04-15 12:59:49 -0700

    fixed some fonts

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -18,8 +18,8 @@ class Header extends React.Component {
   render() {
     return (
       <nav className="d-flex justify-content-space-between navbar header sticky-top">
-        <h2 onClick={this.goToCatalog} className="pointer navbar-brand ml-5">$Wicked Sales</h2>
-        <h2 onClick={this.goToCart} className="pointer navbar-brand mr-5" ><span>
+        <h2 onClick={this.goToCatalog} className="pointer navbar-brand ml-5 text-general">$Wicked Sales</h2>
+        <h2 onClick={this.goToCart} className="pointer navbar-brand mr-5 text-general" ><span>
           {this.props.cartItemCount}</span> Item <i className="fas fa-shopping-cart"></i></h2>
       </nav>
     );
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 class ProductListItem extends React.Component {
   render() {
     return (
-      <div id={this.props.productId} onClick={this.props.goToDetails} className="mobile row pointer card-width m-3 card d-flex align-items-center justify-content-center">
+      <div id={this.props.productId} onClick={this.props.goToDetails} className="mobile row pointer card-width m-3 d-flex align-items-center justify-content-center">
         <div className="card-body p-0 d-flex flex-column justify-content-around">
           <div className="img-container p-2">
             <img className="card-img-top" src={this.props.image} />
           </div>
           <div className="d-flex justify-content-space-between align-items-start flex-column">
-            <h5 className="card-title">{this.props.name}</h5>
+            <h5 className="card-title text-name">{this.props.name}</h5>
             <h6 className="card-subtitle text-muted">{`$ ${(this.props.price / 100).toFixed(2)}`}</h6>
-            <p className="card-text">{this.props.shortDescription}</p>
+            <p className="card-text text-description">{this.props.shortDescription}</p>
           </div>
         </div>
       </div>
