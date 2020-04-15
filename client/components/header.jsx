5eb1f7c07e637322105ab719ddcb99dea6a37556
bailey-hill <bhill1@mail.sfsu.edu>
2020-04-15 12:00:04 -0700

    changed header color and added onclick

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,16 +4,21 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.goToCart = this.goToCart.bind(this);
+    this.goToCatalog = this.goToCatalog.bind(this);
   }
 
   goToCart(event) {
     this.props.setView('cart', {});
   }
 
+  goToCatalog(event) {
+    this.props.setView('catalog', {});
+  }
+
   render() {
     return (
-      <nav className="d-flex justify-content-space-between navbar navbar-dark bg-dark">
-        <h2 className="navbar-brand ml-5">$Wicked Sales</h2>
+      <nav className="d-flex justify-content-space-between navbar header sticky-top">
+        <h2 onClick={this.goToCatalog} className="pointer navbar-brand ml-5">$Wicked Sales</h2>
         <h2 onClick={this.goToCart} className="pointer navbar-brand mr-5" ><span>
           {this.props.cartItemCount}</span> Item <i className="fas fa-shopping-cart"></i></h2>
       </nav>
