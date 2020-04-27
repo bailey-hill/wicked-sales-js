8a9f7c649fe32c83cb387e8091472eeca4cad263
bailey-hill <bhill1@mail.sfsu.edu>
2020-04-27 13:47:31 -0700

    fixed header mobile responsive

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -17,10 +17,10 @@ class Header extends React.Component {
 
   render() {
     return (
-      <nav className="d-flex justify-content-around navbar header sticky-top">
-        <h2 onClick={this.goToCatalog} className="pointer navbar-brand text-header"><i className="short-fall-icon fas fa-stroopwafel">
+      <nav className="d-flex justify-content-around navbar header sticky-top container-fluid flex-row">
+        <h2 onClick={this.goToCatalog} className="pointer navbar-brand text-header col-sm-3 mobile-header"><i className="short-fall-icon fas fa-stroopwafel">
         </i>  Short Fall Brewery</h2>
-        <div className="w-50">
+        <div className="col-sm">
           <div className="alert alert-warning alert-dismissible fade show text-center mb-0" role="alert">
             <strong>This is a demo website! </strong>
           No real purchases will be made.
@@ -29,7 +29,7 @@ class Header extends React.Component {
             </button>
           </div>
         </div>
-        <h2 onClick={this.goToCart} className="pointer navbar-brand text-header" ><span>
+        <h2 onClick={this.goToCart} className="item-text pointer navbar-brand text-header col-sm-3 mobile-header" ><span>
           {this.props.cartItemCount}</span> Item <i className="fas fa-shopping-cart"></i></h2>
       </nav>
     );
