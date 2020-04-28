8ea07571c4843fcc76381c5c13e681ac625725c2
bailey-hill <bhill1@mail.sfsu.edu>
2020-04-28 12:51:56 -0700

    styled form component

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -59,33 +59,33 @@ class CheckoutForm extends React.Component {
   render() {
     return (
       <div>
-        <div className="mt-1 ml-2 text-muted pointer" onClick={this.goToCatalog}> &lt; continue shopping</div>
+        <div className="mt-1 ml-2 text-muted pointer text-general" onClick={this.goToCatalog}> &lt; continue shopping</div>
         <form className="d-flex justify-content-center w-100 align-items-center flex-column form" onSubmit={this.handleSubmit}>
-          <h2>My Cart</h2>
-          <h5 className="text-muted">Order Total: <span>{`$ ${(this.props.totalPrice / 100).toFixed(2)}`}</span></h5>
-          <div className="w-75 submissionRow">
-            <div className="mb-1">Name</div>
+          <h2 className="text-general">My Cart</h2>
+          <h5 className="text-muted text-general">Order Total: <span>{`$ ${(this.props.totalPrice / 100).toFixed(2)}`}</span></h5>
+          <div className="w-75 submissionRow mt-1">
+            <div className="mb-1 text-general">Name</div>
             <input className="form-control" type="text" id="name"
               placeholder="This is a demo website! Do not use personal information."
               onChange={this.handleChange} />
           </div>
-          <div className="w-75 submissionRow">
-            <div className="mb-1">Credit Card</div>
+          <div className="w-75 submissionRow mt-1">
+            <div className="mb-1 text-general">Credit Card</div>
             <input className="form-control" type="text" id="creditCard"
               placeholder="This is a demo website! Do not use personal information."
               onChange={this.handleChange} />
           </div>
-          <div className="w-75 submissionRow">
-            <div className="mb-1">Shipping Address</div>
+          <div className="w-75 submissionRow mt-1">
+            <div className="mb-1 text-general">Shipping Address</div>
             <textarea className="form-control" type="number" id="shippingAddress"
               placeholder="This is a demo website! Do not use personal information."
               onChange={this.handleChange} />
           </div>
-          <div className="d-flex justify-content-start orderButton">
+          <div className="d-flex justify-content-start orderButton w-75">
             <button disabled={!(this.state.name && this.state.creditCard && this.state.shippingAddress)}
-              className="mt-3 mr-3 btn btn-primary" onSubmit={this.handleSubmit}
+              className="mt-3 mr-3 btn btn-primary rounded-pill" onSubmit={this.handleSubmit}
               type="submit" value="Submit">Place Order</button>
-            <input className="mt-3 btn btn-light" type="reset" value="Cancel" />
+            <input className="mt-3 btn btn-light rounded-pill" type="reset" value="Cancel" />
           </div>
         </form>
       </div>
